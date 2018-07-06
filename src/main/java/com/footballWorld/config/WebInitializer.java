ccc199912aad3056f3b4cc2a94deee0ac3765925
yuzdzhanAhmed <ozy7@abv.bg>
2018-07-06 13:58:32 +0300

    spring configuration done

---
+++ src/main/java/com/footballWorld/config/WebInitializer.java
@@ -0,0 +1,43 @@
+package com.footballWorld.config;
+
+import java.io.File;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.servlet.support.
+AbstractAnnotationConfigDispatcherServletInitializer;
+ 
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+ 
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { SpringWebConfig.class };
+    }
+  
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+  
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/", "*.html", "*.pdf" };
+    }
+    private int maxUploadSizeInMb = 50 * 1024 * 1024; // 50 MB
+    
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+
+        // temp file will be uploaded here
+        File uploadDirectory = new File(System.getProperty("java.io.tmpdir"));
+
+        // register a MultipartConfigElement
+        MultipartConfigElement multipartConfigElement =
+                new MultipartConfigElement(uploadDirectory.getAbsolutePath(),
+                        maxUploadSizeInMb, maxUploadSizeInMb * 2, maxUploadSizeInMb / 2);
+
+        registration.setMultipartConfig(multipartConfigElement);
+
+    }
+}
---
+++ src/main/java/com/footballWorld/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.footballWorld.controller;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class HomeController {
+
+
+	@RequestMapping(value = { "/index", "/" }, method = RequestMethod.GET)
+	public String homePage(Model model, HttpServletRequest req)  {
+		
+				return "index";
+	}
+
+	
+
+}
